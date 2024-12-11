fac2601e1aefa0cc4f17dc79d297ab64e498f608
hdfeos <eoshelp@hdfgroup.org>
2024-12-11 09:38:04 -0600

    fix(hermes): version

--- ports/hermes/portfile.cmake
+++ ports/hermes/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO  HDFGroup/hermes
-    REF "${VERSION}"
+    REF "v${VERSION}"
     SHA512 39fd0cff7d866f0d232d6aa889fa1548a9c21a89c04bc198ac0c2010c9ef582efb45f584ce4f21f56637e2f5a357c0f2b3590657ce2ac7e3875fcaf2f0b2bd7f
     HEAD_REF master
     PATCHES
