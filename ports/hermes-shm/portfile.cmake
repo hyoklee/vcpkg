3a54359889d7af874f757252b4f5e3058d9ede5e
hdfeos <eoshelp@hdfgroup.org>
2024-12-11 23:16:35 -0600

    fix(hermes-shm): add patch

--- ports/hermes-shm/portfile.cmake
+++ ports/hermes-shm/portfile.cmake
@@ -4,13 +4,15 @@ vcpkg_from_github(
     REF "v${VERSION}"
     SHA512 83b9253b2172587ac60a75f1501417d42bc0fe7045291a66aad1bb196eb7ff26d67cdd8fafce8fbeb412decd52d82a9df7fefed227a5eb8bcf212c058520bf60
     HEAD_REF master
+    PATCHES
+        random.patch    
 )
 
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DHERMES_ENABLE_DOXYGEN=OFF    
+        -DHERMES_ENABLE_DOXYGEN=OFF
 )
 
 vcpkg_cmake_install()
