ee10a1ca03d85720a6757fee706e25bd4e90c383
H. Joe Lee <hyoklee@hdfgroup.org>
2024-12-12 22:32:19 -0600

    ci(mac): patch sysinfo

--- ports/hermes-shm/portfile.cmake
+++ ports/hermes-shm/portfile.cmake
@@ -5,7 +5,10 @@ vcpkg_from_github(
     SHA512 83b9253b2172587ac60a75f1501417d42bc0fe7045291a66aad1bb196eb7ff26d67cdd8fafce8fbeb412decd52d82a9df7fefed227a5eb8bcf212c058520bf60
     HEAD_REF master
     PATCHES
-        random.patch    
+        random.patch
+        ${IF(VCPKG_TARGET_TRIPLET MATCHES "osx")
+            "sysinfo.patch"
+        ENDIF()}	
 )
 
 
