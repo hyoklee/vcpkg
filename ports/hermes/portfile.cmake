f932cc4f7ffbb9931de119d2bb9a4893c8857497
H. Joe Lee <hyoklee@hdfgroup.org>
2025-11-04 21:37:16 -0600

    feat: iowarp ports

--- ports/hermes/portfile.cmake
+++ ports/hermes/portfile.cmake
@@ -1,31 +1,23 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO  HDFGroup/hermes
+    REPO HDFGroup/hermes
     REF "v${VERSION}"
     SHA512 39fd0cff7d866f0d232d6aa889fa1548a9c21a89c04bc198ac0c2010c9ef582efb45f584ce4f21f56637e2f5a357c0f2b3590657ce2ac7e3875fcaf2f0b2bd7f
-    HEAD_REF master
+    HEAD_REF main
     PATCHES
-        hrun.patch
-	CMakeLists.txt.patch
+    hrun.patch
+    CMakeLists.txt.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        adios HERMES_ENABLE_ADIOS
-        hdf5 HERMES_ENABLE_VFD
-        kvstore HERMES_ENABLE_KVSTORE
-        mpiio HERMES_ENABLE_MPIIO_ADAPTER
-        posix HERMES_ENABLE_POSIX_ADAPTER
-        pubsub HERMES_ENABLE_PUBSUB_ADAPTER
-        python HERMES_ENABLE_PYTHON
-        stdio HERMES_ENABLE_STDIO_ADAPTER
+    hdf5 HERMES_ENABLE_VFD
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        ${FEATURE_OPTIONS}
-
+    ${FEATURE_OPTIONS}
 )
 
 vcpkg_cmake_install()
